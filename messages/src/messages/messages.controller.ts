f520915dc254fd8983979fb1b3fa417ba422c154
Semin Lim <dani416@gmail.com>
2024-09-04 10:10:00 +0900

    2번째 프로젝트 messages 추가
    - nest cli로 작업하기
    - 프로젝트 구조
    - 제어 역전 원칙
    - Dependency Injection(의존성 주입)

---
+++ messages/src/messages/messages.controller.ts
@@ -0,0 +1,30 @@
+import { Controller, Get, Post, Body, Param, NotFoundException } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
+
+@Controller('/messages')
+export class MessagesController {
+    constructor(public messagesService: MessagesService) {
+        // // DONT DO THIS ON REAL APP
+        // // USE DEPENDENCY INJECTION
+        // this.messagesService = new MessagesService();
+    }
+    @Get()
+    listMessages() { // List all messages
+        return this.messagesService.findAll(); // Must RETURN
+    }
+
+    @Post()
+    createMessages(@Body() body: CreateMessageDto) { // Create a message
+        return this.messagesService.create(body.content);
+    }
+
+    @Get('/:id') // Get a particular message
+    async getMessage(@Param('id') id: string) {
+        const message =  await this.messagesService.findOne(id);
+        if( !message ) {
+            throw new NotFoundException('message not found');
+        }
+        return message;
+    }
+}
---
+++ messages/src/messages/messages.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@nestjs/common";
+import { MessagesRepository } from "./messages.repository";
+
+@Injectable()
+export class MessagesService {
+    constructor(public messagesRepo: MessagesRepository) {
+        // // Service is creating its own dependencies
+        // // DONT DO THIS ON REAL APPS
+        // this.messagesRepo = new MessagesRepository();
+    }
+
+    findOne(id: string) {
+        return this.messagesRepo.findOne(id);
+    }
+
+    findAll() {
+        return this.messagesRepo.findAll();
+    }
+
+    create(content: string) {
+        return this.messagesRepo.create(content);
+    }
+}
